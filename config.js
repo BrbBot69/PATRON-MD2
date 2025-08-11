d2a916a53653047fe4cafb34d746c8bdc4edac67
BrbBot69 <148257126+BrbBot69@users.noreply.github.com>
2025-08-11 21:03:49 +0200

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "PATRON-MD~80c2278ed44fadad7c39d7c53a146210",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "false",
 // make true or false status auto seen
