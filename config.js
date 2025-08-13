b718fd2af854dc6b90c2a9428ef6213c37921a0f
BrbBot69 <148257126+BrbBot69@users.noreply.github.com>
2025-08-13 20:22:56 +0200

    Update config.js

--- config.js
+++ config.js
@@ -49,7 +49,7 @@ AUTO_VOICE: process.env.AUTO_VOICE || "false",
 // make true for send automatic voices
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 // make true for automatic stickers 
-AUTO_REPLY: process.env.AUTO_REPLY || "true",
+AUTO_REPLY: process.env.AUTO_REPLY || "false",
 // make true or false automatic text reply 
 ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
 // maks true for always online 
